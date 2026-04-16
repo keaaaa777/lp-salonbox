541710117b4536fe17761309c1eaea2c22e15240
keaaaa777 <61.monosix@gmail.com>
2026-04-17 08:34:10 +0900

    ブログとLPを分離

--- blog-next/src/app/common/page.tsx
+++
@@ -1,7 +0,0 @@
-import CategoryListing from "../../components/CategoryListing";
-import { getAllPosts } from "../../lib/posts";
-
-export default function SalonboxCategoryPage() {
-  const posts = getAllPosts().filter((post) => post.category === "common");
-  return <CategoryListing label="共通" path="/common/" posts={posts} />;
-}
